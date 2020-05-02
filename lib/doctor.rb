a093f08899e8e3c29e33112f4d84c84f97bbcd66
Amal Abdu <abduamal@ymail.com>
2020-05-02 18:18:21 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,11 +8,12 @@ class Doctor
     @@all << self
   end
 
-  def self.all
-    @@all
-  end
-
   def appointments
     Appointment.all.select { |appointment| appointment.doctor == self}
   end
+
+  def self.all
+    @@all
+  end
+  
 end
